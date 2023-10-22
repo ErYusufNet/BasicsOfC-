a68190027a7a0f0965f926ecfefdab104da89978
ErYusufNet <yusufer.net@hotmail.com>
2023-10-22 22:04:45 +0300

    updatex2

--- Practice1/Program.cs
+++ Practice1/Program.cs
@@ -10,11 +10,18 @@ namespace Practice1
     {
         static void Main(string[] args)
         {
-            for(int i = 1; i<=10; i++)
+            for (int i = 1; i <= 10; i++)
             {
+                // Dış döngü: Bu döngü, i adında bir tamsayı değişkenini 1'den 10'a kadar artırarak çalışır.
+
                 for (int j = 1; j <= 10; j++)
                 {
-                    Console.WriteLine(i + "x" +j+"="+(i * j));
+                    // İç içe geçmiş bir döngü: j adında bir tamsayı değişkenini 1'den 10'a kadar artırarak çalışır.
+
+                    Console.WriteLine(i + "x" + j + "=" + (i * j));
+                    // Console.WriteLine, ekrana bir metin çıktısı gönderir.
+                    // Bu satırda i ve j değişkenlerinin değerleri çarpılır ve sonuç metin haline getirilir.
+                    // Örneğin, i=2 ve j=3 için "2x3=6" gibi bir çıktı elde edilir.
                 }
             }
             Console.ReadLine();
